505561b6a8a486bc1a0897128f832f770d1f212e
Dale O'Brien <daleobrien@users.noreply.github.com>
2024-10-23 21:46:12 +1100

    Better formatting

--- src/pretty_print.rs
+++ src/pretty_print.rs
@@ -5,12 +5,8 @@ use tabled::builder::Builder;
 use tabled::settings::object::Rows;
 use tabled::settings::{Alignment, Style};
 
-fn pad_string(s: &str, len: usize) -> String {
-    let mut s = s.to_string();
-    while s.len() < len {
-        s.push(' ');
-    }
-    s
+pub fn left_pad(s: &str, n: usize) -> String {
+    format!("{:>width$}", s, width = n)
 }
 
 pub fn print_data_as_table(
@@ -20,18 +16,21 @@ pub fn print_data_as_table(
     user_to_user_pr_count: HashMap<(String, String), u32>,
 ) {
     let mut stats_builder = Builder::default();
-    let stats_header = vec!["User", "PRs", "Approved", "Comments"];
+    let stats_header = vec!["User", "PRs [%]", "Approved [%]", "Comments [%]"];
+    let max_len = stats_header.iter().map(|u| u.len()).max().unwrap();
+    let stats_header = stats_header
+        .iter()
+        .map(|u| left_pad(u, max_len))
+        .collect::<Vec<String>>();
     stats_builder.push_record(stats_header);
 
     // Find the longest username for formatting
     let max_user_len = all_users.iter().map(|u| u.len()).max().unwrap();
 
     let mut relationship_builder = Builder::default();
-    let mut relationship_header = vec!["Reviewer\\Author"];
-    for user in all_users {
-        let padded = pad_string(user, max_user_len);
-        relationship_header.push(user);
-    }
+    let mut relationship_header:Vec<String> = vec!["Reviewer\\Author".to_string()];
+    let header = all_users.iter().map(|u| left_pad(u, max_user_len)).collect::<Vec<String>>();
+    relationship_header.extend(header);
     relationship_builder.push_record(relationship_header);
 
     let total_prs = user_data
@@ -76,7 +75,7 @@ pub fn print_data_as_table(
         ];
         stats_builder.push_record(row);
 
-        let mut relationship_row = vec![pad_string(user, max_user_len)];
+        let mut relationship_row = vec![user.to_string()];
         for user2 in all_users {
             let c = user_to_user_pr_count.get(&(user.clone(), user2.clone()));
             if user == user2 {
@@ -88,7 +87,7 @@ pub fn print_data_as_table(
         relationship_builder.push_record(relationship_row);
     }
 
-    println!("Statistics [%]:");
+    println!("\nStatistics:");
     let table = stats_builder
         .build()
         .with(Style::rounded())
@@ -96,7 +95,7 @@ pub fn print_data_as_table(
         .to_string();
     println!("{table}");
 
-    println!("Relationships [count]:");
+    println!("\nRelationships [count]:");
     let table = relationship_builder
         .build()
         .with(Style::rounded())
